4e21bf73377603f366731383ac6ed9b12d468ae0
Neylson Crepalde <neylson.crepalde@a3data.com.br>
2021-09-07 14:30:44 -0300

    api exemplo e Dockerfiles

---
+++ docker/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "You are doing fine with FastAPI..."}
+
+
+@app.get("/api/{name}")
+async def return_name(name):
+    return {
+        "name": name,
+        "message": f"Hello, {name}!"
+    }
