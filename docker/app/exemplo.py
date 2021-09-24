700b40cfc80be35fb3e2c1a52ed0df109d767a81
Neylson Crepalde <neylson.crepalde@a3data.com.br>
2021-09-23 22:34:30 -0300

    api deployment

--- docker/app/exemplo.py
+++ docker/app/exemplo.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
@@ -12,4 +13,16 @@ async def return_name(name):
     return {
         "name": name,
         "message": f"Hello, {name}!"
+    }
+
+
+@app.get("/piada")
+async def return_piada():
+    piadas = [
+        "O que o cachorro faz quando o dono o chama? Late!",
+        "O que cai de pé e corre deitado? Minhoca de pára-quedas!",
+        "Em uma corrida de frutas a maçã está ganhando. O que acontece se ela diminuir a velocidade? A uva passa."
+    ]
+    return {
+        "piada": random.choice(piadas)
     }
